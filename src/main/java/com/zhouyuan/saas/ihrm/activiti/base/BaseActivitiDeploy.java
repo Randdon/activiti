a9ac0661279d51fff84f844c3e91e8f78da522f5
yuandong <yuand@drplant.com.cn>
2019-10-11 14:50:17 +0800

    提交以zip压缩包方式部署流程的方式

--- src/main/java/com/zhouyuan/saas/ihrm/activiti/base/BaseActivitiDeploy.java
+++ src/main/java/com/zhouyuan/saas/ihrm/activiti/base/BaseActivitiDeploy.java
@@ -5,6 +5,9 @@ import org.activiti.engine.ProcessEngines;
 import org.activiti.engine.RepositoryService;
 import org.activiti.engine.repository.Deployment;
 
+import java.io.InputStream;
+import java.util.zip.ZipInputStream;
+
 /**
  * 流程定义部署
  */
@@ -12,6 +15,7 @@ public class BaseActivitiDeploy {
 
     public static void main(String[] args) {
 
+        deployByZip();
     }
 
     /**
@@ -32,6 +36,31 @@ public class BaseActivitiDeploy {
         Deployment deployment = repositoryService.createDeployment()
                 .addClasspathResource("diagram/holiday.bpmn")//添加bpmn资源
                 .addClasspathResource("diagram/holiday.png")//添加png资源
+                .name("请假流程")
+                .deploy();
+
+        //输出部署信息
+        System.out.println(deployment.getId());
+        System.out.println(deployment.getName());
+
+    }
+
+    public static void deployByZip(){
+
+        //创建processEngine对象
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+
+        //生成repositoryService实例
+        RepositoryService repositoryService = processEngine.getRepositoryService();
+
+        //构造zip输入流
+        InputStream inputStream = BaseActivitiDeploy.class.getClassLoader()
+                .getResourceAsStream("diagram/holiday.zip");
+        ZipInputStream zipInputStream = new ZipInputStream(inputStream);
+
+        //进行部署
+        Deployment deployment = repositoryService.createDeployment()
+                .addZipInputStream(zipInputStream)
                 .name("请假流程")
                 .deploy();
 
